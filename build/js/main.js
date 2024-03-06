555a5aa799b04f4de32c3fdec3bdfb838118f02b
coding-jhuunn <codingatjhunn@gmail.com>
2024-03-06 15:19:27 +0800

    done with the function

--- build/js/main.js
+++ build/js/main.js
@@ -1,2 +1,20 @@
 "use strict";
-console.log("hello");
+const inputValue = document.getElementById("input");
+const checkBtn = document.getElementById("check-btn");
+const result = document.getElementById("result");
+checkBtn === null || checkBtn === void 0 ? void 0 : checkBtn.addEventListener("click", () => {
+    if (inputValue.value.length < 1) {
+        result.innerHTML = "Please enter a word";
+    }
+    else {
+        const input = inputValue.value;
+        const toCheck = input.split("").reverse().join("");
+        if (toCheck === input) {
+            result.innerHTML = `${input} is an example of palindrome word`;
+        }
+        else {
+            result.innerHTML = `${input} is not an example of palindrome word`;
+        }
+        inputValue.value = "";
+    }
+});
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
-console.log("hello");
+const inputValue = document.getElementById("input") as HTMLInputElement;
+
+const checkBtn = document.getElementById("check-btn");
+
+const result = document.getElementById("result") as HTMLInputElement;
+
+checkBtn?.addEventListener("click", () => {
+  if (inputValue.value.length < 1) {
+    result.innerHTML = "Please enter a word";
+  } else {
+    const input = inputValue.value;
+
+    const toCheck = input.split("").reverse().join("");
+    if (toCheck === input) {
+      result.innerHTML = `${input} is an example of palindrome word`;
+    } else {
+      result.innerHTML = `${input} is not an example of palindrome word`;
+    }
+    inputValue.value = "";
+  }
+});
